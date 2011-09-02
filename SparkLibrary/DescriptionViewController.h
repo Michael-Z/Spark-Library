01ffc41c12696d3cf1f4fceb6bfd30e7c457d22b
emadhegb <emadhegab@hotmail.com>
2011-09-02 21:16:12 +0200

    add description

---
+++ SparkLibrary/DescriptionViewController.h
@@ -0,0 +1,13 @@
+//
+//  DescriptionViewController.h
+//  SparkLibrary
+//
+//  Created by Mohamed Hegab on 9/2/11.
+//  Copyright 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DescriptionViewController : UIViewController
+
+@end
